785f81313a22a5acd89f8271a6e4f7540a970638
Krystian <krystian.easyripok@gmail.com>
2023-01-17 22:57:30 +0100

    Zadanie 3 i 4

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,32 @@
+import java.util.Date;
+
 public class Human extends Animal {
     String firstName;
     String lastName;
     Car car;
-    Double salary;
+    private Double salary;
 
     public Human(String firstName) {
         super("homo sapiens");
         this.weight = 70.0;
         this.salary = 0.0;
         this.firstName = firstName;
+    }
+
+    public Double getSalary() {
+        Date date = new Date();
+        System.out.println("Data pobrania wypłaty: " + date + " Wartość wypłaty: " + salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary < 0) {
+            System.out.println("Wartość wypłaty jest ujemna!");
+        } else {
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego.");
+            System.out.println("Jest konieczności odebrania aneksu do umowy od pani Hani z kadr.");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu.");
+            this.salary = salary;
+        }
     }
 }
